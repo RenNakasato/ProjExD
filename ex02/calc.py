7b982595207ebb0c300520dcd1616c986147248b
nakasato <c0b2111962@edu.teu.ac.jp>
2022-10-04 14:58:26 +0900

    その４

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import right
 
 def button_click(event):
     btn = event.widget
@@ -9,13 +10,20 @@ def button_click(event):
 root = tk.Tk()
 root.geometry("300x500")
 
-r,c=0,0
+entry = tk.Entry(root,
+                 width=10,
+                 font=("Times New Roman",40),
+                 justify="right")
+
+entry.grid(row=0,column=0,columnspan=4)
+
+r,c=1,0
 for i in range(9,-1,-1):
     button = tk.Button(root,text=i,font=("Times New Roman", 30),height=2,width=4)
     button.grid(row=r,column=c)
     button.bind("<1>",button_click)
     c +=1
-    if c ==2:
+    if c ==3:
         r +=1
         c = 0
 

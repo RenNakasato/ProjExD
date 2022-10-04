fee95ba2a02ab4cd9806eb415f1614794d19ca6e
nakasato <c0b2111962@edu.teu.ac.jp>
2022-10-04 14:44:15 +0900

    二つ目

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,7 +1,17 @@
+from cgitb import text
 import tkinter as tk
 import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.geometry("300x500")
+
+r,c=0,0
+for i in range(9,-1,-1):
+    button = tk.Button(root,text=i,font=("Times New Roman", 30),height=2,width=4)
+    button.grid(row=r,column=c)
+    c +=1
+    if c ==2:
+        r +=1
+        c = 0
 
 root.mainloop()

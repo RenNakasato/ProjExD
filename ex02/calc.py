8849d91af9e876726ac52536dbe75482e4e44ab4
nakasato <c0b2111962@edu.teu.ac.jp>
2022-10-04 14:49:53 +0900

    その３

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,10 @@
-from cgitb import text
 import tkinter as tk
 import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"{txt}が押されました")
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -9,6 +13,7 @@ r,c=0,0
 for i in range(9,-1,-1):
     button = tk.Button(root,text=i,font=("Times New Roman", 30),height=2,width=4)
     button.grid(row=r,column=c)
+    button.bind("<1>",button_click)
     c +=1
     if c ==2:
         r +=1

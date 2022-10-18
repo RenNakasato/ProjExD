feba51632e8894281e2e36849adf7f5267c77a0a
nakasato <c0b2111962@edu.teu.ac.jp>
2022-10-18 18:05:54 +0900

    コメントアウト #8

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,10 +4,18 @@ import tkinter.messagebox as tkm
 import datetime
 import random
 
+#全グローバル変数の説明
+#key, mazu,
+#どのキーが押されたかを記録、壁と床の判定、
+#mx, my, cx,cy,
+#キャラクターの移動関係*2、キャラクターの実際の位置*2
+#st, gt
+#ゲームモードの開始時間、ゲームモードの終了時間
 
 def key_down(event):
-    global key, maze, mx, my, cx,cy, st, gt
+    global key, mx, my, cx,cy, st, gt
     key = event.keysym
+    # 何かあった時に全てを初期状態に変更する
     if key == "c":
         mx, my = 1, 1
         cx, cy = mx*150, my*150
@@ -15,6 +23,7 @@ def key_down(event):
         canv.coords("tori", cx, cy)
         canv.create_rectangle(100, 0, 200, 100, fill ="gray")
         canv.create_rectangle(1300, 800, 1400, 900, fill = "gray")
+    #　ゲームモードの起動、青から赤に行くとクリアー
     if key=="g":
         mx, my = 1, 1
         cx, cy = mx*150, my*150
@@ -25,13 +34,14 @@ def key_down(event):
         canv.create_text(1350,850, text="G",fill="white", font=("", 50))
         st = datetime.datetime.now()
 
-
+#　ゲームモードのクリアの時間
 def gaol():
     global mx, my, cx, cy, maze, st, gt
     if cx >= 1300 and cx < 1500 and cy >= 700 and cy < 900 and st != 0:
         gt = datetime.datetime.now()
         time = gt -st
         tkm.showinfo("おめでとう", f"{time}でクリアしました")
+        #　tkmを使うことによりtkmをどうにかするまでで次の動きを封じる
         canv.create_rectangle(100, 0, 200, 100, fill ="gray")
         canv.create_rectangle(1300, 800, 1400, 900, fill = "gray")
         st = 0
@@ -41,7 +51,7 @@ def key_up(event):
     global key
     key = ""
 
-
+#　矢印キーの動きとゲームモード時のクリア判定
 def main_proc():
     global mx, my, cx, cy, maze,st, gt
     yazirusi = ["Up", "Down", "Left", "Right"]
@@ -74,6 +84,7 @@ if __name__ == "__main__":
     maze = mm.make_maze(15,9)
     mm.show_maze(canv, maze)
 
+    #　起動時にランダムなこうかとんの画像にする
     num = random.randint(0, 8)
     tori = tk.PhotoImage(file=f"fig/{num}.png")#3
     mx, my = 1, 1

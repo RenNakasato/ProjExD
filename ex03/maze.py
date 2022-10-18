b5c45e0c4f32d0ab473feb6408e98bc82d9a5693
nakasato <c0b2111962@edu.teu.ac.jp>
2022-10-18 15:30:30 +0900

    12

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,15 +13,20 @@ def key_up(event):
 
 
 def main_proc():
-    global cx, cy, key
+    global mx, my
+    global cx, cy
     if key =="Up":
-        cy -=20
+        my -= 1
     if key =="Down":
-        cy += 20
+        my +=1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    if maze[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        mx,my = int((cx-50)/100),int((cy-50)/100)
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -32,18 +37,20 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")#2
     canv.pack()
 
-    tori = tk.PhotoImage(file="fig/5.png")#3
-    cx, cy = 300, 400
-    canv.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""#4,5
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
 
-    main_proc()#7
-
     #9
     maze = mm.make_maze(15,9)
     mm.show_maze(canv, maze)
+
+    tori = tk.PhotoImage(file="fig/5.png")#3
+    mx, my = 1, 1
+    cx, cy = mx*150, my*150
+    canv.create_image(cx, cy, image=tori, tag="tori")
+    main_proc()#7
+
 
     root.mainloop()

10a5cdb79dbadf500fb621ebac7485339a7453ab
nakasato <c0b2111962@edu.teu.ac.jp>
2022-10-25 14:42:04 +0900

    問題０４-4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -11,7 +11,7 @@ def main():#1
     tori_sfc = pg.image.load("./fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
-    tori_rct = 900, 400
+    tori_rct.center = 900, 400
 
     Clock = pg.time.Clock()
 
@@ -20,9 +20,15 @@ def main():#1
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-
+        
+        key_stats = pg.key.get_pressed()#4
+        if key_stats[pg.K_UP]: tori_rct.centery -=1 
+        if key_stats[pg.K_DOWN]:tori_rct.centery +=1 
+        if key_stats[pg.K_LEFT]:tori_rct.centerx -=1
+        if key_stats[pg.K_RIGHT]:tori_rct.centerx +=1 
         scrn_sfc.blit(tori_sfc,tori_rct)
         pg.display.update()
+        if key_stats[pg.K_ESCAPE]:break
 
         Clock.tick(1000)
 
